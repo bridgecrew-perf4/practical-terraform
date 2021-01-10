a7a8b94481254c4af49aed28915a0ee1f3dc6ca6
ikhrnet <ikhrnet@users.noreply.github.com>
2021-01-11 01:05:38 +0900

    Use variable and local variable

--- example/main.tf
+++ example/main.tf
@@ -1,11 +1,19 @@
+variable "profile" {
+  default = "ikhrnet"
+}
+
+locals {
+  example_instance_type = "t3.nano"
+}
+
 provider "aws" {
   region  = "us-east-1"
-  profile = "ikhrnet"
+  profile = var.profile
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-0be2609ba883822ec"
-  instance_type = "t3.nano"
+  instance_type = local.example_instance_type
 
   tags = {
     Name = "example"
