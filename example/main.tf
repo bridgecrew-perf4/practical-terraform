a4d7b72ac2614a549c0768f53f143da4a7f0bcb2
ikhrnet <ikhrnet@users.noreply.github.com>
2021-01-11 01:23:43 +0900

    Apply filters to get latest AMI ID

--- example/main.tf
+++ example/main.tf
@@ -11,8 +11,23 @@ provider "aws" {
   profile = var.profile
 }
 
+data "aws_ami" "recent_amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.????????-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-0be2609ba883822ec"
+  ami           = data.aws_ami.recent_amazon_linux_2.image_id 
   instance_type = local.example_instance_type
 
   tags = {
