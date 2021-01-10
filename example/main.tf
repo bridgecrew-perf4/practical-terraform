36e386faaf694563bd00109b80770ac8f2e386e3
ikhrnet <ikhrnet@users.noreply.github.com>
2021-01-11 02:40:16 +0900

    Create security group to allow access via 80

--- example/main.tf
+++ example/main.tf
@@ -27,20 +27,43 @@ data "aws_ami" "recent_amazon_linux_2" {
 }
 
 resource "aws_instance" "example" {
-  ami           = data.aws_ami.recent_amazon_linux_2.image_id 
-  instance_type = local.example_instance_type
+  ami                    = data.aws_ami.recent_amazon_linux_2.image_id
+  instance_type          = local.example_instance_type
+  vpc_security_group_ids = [aws_security_group.example_ec2.id]
 
   tags = {
     Name = "example"
   }
 
   user_data = <<EOF
-    #/bin/bash
+    #!/bin/bash
     yum install -y httpd
     systemctl start httpd.service
 EOF
 }
 
+resource "aws_security_group" "example_ec2" {
+  name = "example-ec2"
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 output "example_instance_id" {
   value = aws_instance.example.id
+}
+
+output "example_public_dns" {
+  value = "http://${aws_instance.example.public_dns}"
 }
